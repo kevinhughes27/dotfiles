556ea36ac2a00f3b4256a951bcbb9f2e2c9127ad
Kevin Hughes <kevin.hughes@shopify.com>
2013-12-10 14:18:49 -0500

    added tmux.conf

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ echo "Backup the old files"
 mv ~/.bashrc ~/.bashrc.old
 mv ~/.bash_profile ~/.bash_profile.old
 mv ~/.bash ~/.bash.old
+mv ~/.tmux.conf ~/.tmux.conf.old
 mv ~/.gitconfig ~/.gitconfig.old
 mv ~/.gitignore ~/.gitignore.old 
 mv ~/.vimrc ~/.vimrc.old
@@ -13,6 +14,7 @@ echo "Symlinking files"
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/dotfiles/bash ~/.bash
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/gitignore ~/.gitignore
 ln -s ~/dotfiles/vimrc ~/.vimrc
