d0977363042c1afc5eb20b245e02772bb9348a88
Kevin Hughes <kevin.hughes@shopify.com>
2013-12-10 15:50:28 -0500

    added a git push alias/macro

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -12,7 +12,33 @@ alias zt='zeus test'
 
 # Rake Commands
 alias rt='rake test'
-alias rtp="PARALLEL=1 PRIDE=1 rake test"
+alias rtp='PARALLEL=1 PRIDE=1 rake test'
+
+# Git Commands
+function gp {
+  local git_status="`git status -unormal 2>&1`"
+  if ! [[ "$git_status" =~ Not\ a\ git\ repo ]]; then
+    if [[ "$git_status" =~ On\ branch\ ([^[:space:]]+) ]]; then
+      local branch=${BASH_REMATCH[1]}
+      if [[ "$branch" =~ "master" ]]; then
+        read -p "push master, Are you sure? " -n 1 -r
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+          echo
+          echo "$(git push origin) $branch"
+        else
+          echo
+          echo 'git push aborted.'
+        fi  
+      else
+        echo "$(git push origin) $branch"
+      fi
+    else
+      echo 'not on a branch'
+    fi
+  else
+    echo 'Not a git repo'
+  fi
+}
 
 # eg. alias e=echo
 alias gitfix='git rebase -i HEAD~2'
--- install.sh
+++ install.sh
@@ -19,4 +19,7 @@ ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/gitignore ~/.gitignore
 ln -s ~/dotfiles/vimrc ~/.vimrc
 
+echo "Reloading"
+exec bash
+
 echo "All done."
