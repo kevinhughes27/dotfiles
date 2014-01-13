264b3a0d3f95f6f0d2b05aa65950a1b4d6de6205
Kevin Hughes <kevin.hughes@shopify.com>
2014-01-13 13:21:20 -0500

    updated sublime settings install on osx

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@ echo "Backup the original files"
 backup() {
   # backs up the file/folder the first time only
   file="$1"
-  if [[ -f $file ]]; then  
+  if [[ -f $file ]]; then
     if [[ ! -f "$file.old" ]]; then
       mv $file "$file.old"
     fi
   elif [[ -d $file ]]; then
     if [[ ! -d "$file.old" ]]; then
       mv $file "$file.old"
-    fi 
+    fi
   fi
 }
 
@@ -46,7 +46,7 @@ link ~/dotfiles/tmux.conf ~/.tmux.conf
 link ~/dotfiles/gitconfig ~/.gitconfig
 link ~/dotfiles/gitignore ~/.gitignore
 link ~/dotfiles/vimrc ~/.vimrc
-link ~/dotfiles/sublime/Packages/User/Preferences.sublime-settings $sublime_path
+link ~/dotfiles/sublime/Packages/User/Preferences.sublime-settings "$sublime_path"
 
 echo "All done."
 
