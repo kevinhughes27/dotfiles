3f03946b94d3e3b8f25c65b9ced1f116f8b92aca
Kevin Hughes <kevinhughes27@gmail.com>
2020-01-22 19:52:06 -0500

    add curl timing helper

--- shell/functions.sh
+++ shell/functions.sh
@@ -68,7 +68,20 @@ function ruby_test {
   fi
 }
 
-# start python simple http server in the working directory
+# serve files from current directory
 function server() {
   python -m SimpleHTTPServer 8000
+}
+
+# print timing for various stages of an HTTP connection to a domain
+function curl_time() {
+  curl -so /dev/null -w "\
+   namelookup:  %{time_namelookup}s\n\
+      connect:  %{time_connect}s\n\
+   appconnect:  %{time_appconnect}s\n\
+  pretransfer:  %{time_pretransfer}s\n\
+     redirect:  %{time_redirect}s\n\
+starttransfer:  %{time_starttransfer}s\n\
+-------------------------\n\
+        total:  %{time_total}s\n" "$@"
 }
