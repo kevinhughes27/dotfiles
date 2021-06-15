57c5aa8366e4dc3a3964bb0ff1e2c0ee6f24be81
Kevin Hughes <kevinhughes27@gmail.com>
2021-06-15 11:56:24 -0400

    ignore ds store

--- nvim/lua/tree-config.lua
+++ nvim/lua/tree-config.lua
@@ -1,6 +1,6 @@
 local g = vim.g
 
-g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
+g.nvim_tree_ignore = {".git", "node_modules", ".cache", ".DS_Store"}
 g.nvim_tree_width = 30
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_show_icons = {
