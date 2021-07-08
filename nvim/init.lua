3b30e6f88c787d77a0ca174723b0c8a4e8943655
Kevin Hughes <kevinhughes27@gmail.com>
2021-07-08 14:33:31 -0400

    dont need this include hack anymore. deps have been fixed

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,5 @@
 ----------------------- Helpers -------------------------------
 local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
-local lsp = vim.lsp  -- had to add this here or it errors when other lua files try and grab it
 local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
 local g = vim.g      -- a table to access global variables
 local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
