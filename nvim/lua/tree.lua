e094d9384b61064eb5df3c714273fee2cb93180d
Kevin Hughes <kevinhughes27@gmail.com>
2021-10-02 17:42:27 -0400

    migrate tree config

--- nvim/lua/tree.lua
+++ nvim/lua/tree.lua
@@ -1,36 +1,37 @@
-local g = vim.g
+local tree_cb = require("nvim-tree.config").nvim_tree_callback
 
-g.nvim_tree_ignore = {".git", "node_modules", ".cache", ".DS_Store"}
-g.nvim_tree_width = 30
-g.nvim_tree_auto_resize = 0
-g.nvim_tree_indent_markers = 1
-g.nvim_tree_show_icons = {
+require('nvim-tree').setup {
+  view = {
+    width = 30,
+    side = 'left',
+    auto_resize = false,
+    mappings = {
+      custom_only = true,
+      list = {
+        {key = {"<CR>"},           cb = tree_cb("edit")},
+        {key = {"<2-LeftMouse>"},  cb = tree_cb("edit")},
+        {key = {"<2-RightMouse>"}, cb = tree_cb("cd")},
+        {key = {"<C-t>"},          cb = tree_cb("tabnew")},
+        {key = {"R"},              cb = tree_cb("refresh")},
+        {key = {"a"},              cb = tree_cb("create")},
+        {key = {"d"},              cb = tree_cb("remove")},
+        {key = {"r"},              cb = tree_cb("rename")},
+        {key = {"<C-r>"},          cb = tree_cb("full_rename")},
+        {key = {"x"},              cb = tree_cb("cut")},
+        {key = {"c"},              cb = tree_cb("copy")},
+        {key = {"p"},              cb = tree_cb("paste")},
+        {key = {"q"},              cb = tree_cb("close")},
+      }
+    }
+  }
+}
+
+vim.g.nvim_tree_ignore = {".git", "node_modules", ".cache", ".DS_Store"}
+vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_show_icons = {
   folders = 1,
   files = 1,
   git = 0
-}
-
--- Remove all default bindings.
-g.nvim_tree_disable_default_keybindings = 1
-
--- Add back most of the default bindings
--- but not `-` which I use to resize splits
-local tree_cb = require("nvim-tree.config").nvim_tree_callback
-
-g.nvim_tree_bindings = {
-  {key = {"<CR>"},           cb = tree_cb("edit")},
-  {key = {"<2-LeftMouse>"},  cb = tree_cb("edit")},
-  {key = {"<2-RightMouse>"}, cb = tree_cb("cd")},
-  {key = {"<C-t>"},          cb = tree_cb("tabnew")}, -- this is now opening the window picker which I don't want. How can I new tab open now?
-  {key = {"R"},              cb = tree_cb("refresh")},
-  {key = {"a"},              cb = tree_cb("create")},
-  {key = {"d"},              cb = tree_cb("remove")},
-  {key = {"r"},              cb = tree_cb("rename")},
-  {key = {"<C-r>"},          cb = tree_cb("full_rename")},
-  {key = {"x"},              cb = tree_cb("cut")},
-  {key = {"c"},              cb = tree_cb("copy")},
-  {key = {"p"},              cb = tree_cb("paste")},
-  {key = {"q"},              cb = tree_cb("close")},
 }
 
 -- sync the tree but only on open
