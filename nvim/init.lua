1360e9411c8d3c4621e4a8e037a4d9337e367156
Kevin Hughes <kevinhughes27@gmail.com>
2021-10-02 17:58:39 -0400

    remove oc script. I only use this from vim and I don't find the popup useful when connection from a shell

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
 ----------------------- Helpers -------------------------------
-local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
+local fn = vim.fn    -- to call vim functions e.g. fn.bufnr()
 local g = vim.g      -- a table to access global variables
 local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
 
