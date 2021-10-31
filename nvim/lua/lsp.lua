950b014ffee8a23a2036439ea96c92239803d1e3
Kevin Hughes <kevinhughes27@gmail.com>
2021-10-31 15:58:49 -0400

    shorten comment

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,6 +1,6 @@
 local lspconf = require("lspconfig")
 
--- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+-- nvim-cmp capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
