9fb3efb38ee4cd170e0e5a863c6952a277f8d2d2
Kevin Hughes <kevinhughes27@gmail.com>
2021-10-31 15:40:06 -0400

    refactor es_ruby config. add python lsp

---
+++ nvim/lua/es_ruby.lua
@@ -0,0 +1,38 @@
+-- define custom language server
+local configs = require "lspconfig/configs"
+local util = require "lspconfig/util"
+
+configs.es_ruby = {
+  default_config = {
+    cmd = { "nc", "localhost", "8341" },
+    filetypes = { "ruby", },
+    root_dir = util.root_pattern("Gemfile", ".git"),
+  },
+  docs = {
+    description = [[
+      Language server for Ruby, backed by Elastic search.
+    ]],
+    default_config = {
+      root_dir = [[root_pattern("Gemfile", ".git")]],
+    },
+  },
+}
+
+-- manual setup:
+-- docker pull blinknlights/elastic_ruby_server
+-- docker volume create elastic_ruby_server-0.2.0 (this could be anything but for now match the vscode extension)
+--[[
+  docker run \
+    -d \
+    --rm \
+    --name elastic-ruby-server \
+    --ulimit memlock=-1:-1 \
+    -v elastic_ruby_server-0.2.0:/usr/share/elasticsearch/data \
+    -p 8341:8341 \
+    -e SERVER_PORT=8341 \
+    -e LOG_LEVEL=DEBUG \
+    -e HOST_PROJECT_ROOTS="/Users/kevinhughes/clio" \
+    --mount "type=bind,source=/Users/kevinhughes/clio,target=/projects/clio,readonly" \
+    blinknlights/elastic_ruby_server
+]]
+
--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,23 +1,3 @@
--- custom language servers
-local configs = require "lspconfig/configs"
-local util = require "lspconfig/util"
-
-configs.es_ruby = {
-  default_config = {
-    cmd = { "nc", "localhost", "8341" },
-    filetypes = { "ruby", },
-    root_dir = util.root_pattern("Gemfile", ".git"),
-  },
-  docs = {
-    description = [[
-      Language server for Ruby, backed by Elastic search.
-    ]],
-    default_config = {
-      root_dir = [[root_pattern("Gemfile", ".git")]],
-    },
-  },
-}
-
 local lspconf = require("lspconfig")
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
@@ -53,29 +33,30 @@ lspconf["rust_analyzer"].setup({
   on_attach = on_attach
 })
 
--- docker pull blinknlights/elastic_ruby_server
--- docker volume create elastic_ruby_server-0.2.0 (this could be anything but for now match the vscode extension)
---[[
-  docker run \
-    -d \
-    --rm \
-    --name elastic-ruby-server \
-    --ulimit memlock=-1:-1 \
-    -v elastic_ruby_server-0.2.0:/usr/share/elasticsearch/data \
-    -p 8341:8341 \
-    -e SERVER_PORT=8341 \
-    -e LOG_LEVEL=DEBUG \
-    -e HOST_PROJECT_ROOTS="/Users/kevinhughes/clio" \
-    --mount "type=bind,source=/Users/kevinhughes/clio,target=/projects/clio,readonly" \
-    blinknlights/elastic_ruby_server
-]]
-lspconf["es_ruby"].setup({
+-- npm install -g pyright
+lspconf["pyright"].setup({
+  capabilities = capabilities,
+  on_attach = on_attach
+})
+
+-- pip install 'python-lsp-server[all]'
+lspconf["pylsp"].setup({
   capabilities = capabilities,
   on_attach = on_attach
 })
 
 -- npm install -g bash-language-server
--- lspconf["bashls"].setup({ on_attach = on_attach })
+lspconf["bashls"].setup({
+  capabilities = capabilities,
+  on_attach = on_attach
+})
+
+
+-- require("es_ruby")
+-- lspconf["es_ruby"].setup({
+--   capabilities = capabilities,
+--   on_attach = on_attach
+-- })
 
 require("lspkind").init({
   symbol_map = {
