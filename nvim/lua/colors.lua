c552dd2d3494a78c93d0d6d6827b84d8db5af2f3
Kevin Hughes <kevinhughes27@gmail.com>
2021-12-19 11:14:24 -0500

    custom cmp colors

--- nvim/lua/colors.lua
+++ nvim/lua/colors.lua
@@ -17,3 +17,15 @@ vim.cmd('highlight PmenuSel guibg=' .. c.green)
 
 -- set selected tab highlight to blue
 vim.cmd('highlight TabLineSel guibg=' .. c.blue)
+
+
+-- cmp
+vim.cmd('highlight! CmpItemAbbrMatch guifg=' .. '#569CD6')
+vim.cmd('highlight! CmpItemAbbrMatchFuzzy guifg=' .. '#569CD6')
+
+vim.cmd('highlight! CmpItemKindClass guifg=' .. c.cyan)
+vim.cmd('highlight! CmpItemKindMethod guifg=' .. c.blue)
+vim.cmd('highlight! CmpItemKindVariable guifg=' .. c.purple)
+vim.cmd('highlight! CmpItemKindFunction guifg=' .. c.blue)
+vim.cmd('highlight! CmpItemKindKeyword guifg=' .. c.red)
+vim.cmd('highlight! CmpItemKindText guifg=' .. c.fg)
