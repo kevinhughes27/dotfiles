6d6f3ba397f484d4dad99222cfe06201a07add4f
Kevin Hughes <kevin.hughes@aiven.io>
2022-04-07 12:44:45 -0400

    tidy lsp config

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -44,46 +44,48 @@ lsp_installer.on_server_ready(function(server)
   server:setup(opts)
 end)
 
--- local lspconf = require('lspconfig')
 
--- npm install -g typescript-language-server
--- lspconf['tsserver'].setup({
---   capabilities = capabilities,
---   on_attach = on_attach
--- })
+-- elastic_ruby_server
+----------------------
 
--- GO111MODULE=on go get golang.org/x/tools/gopls@latest
--- lspconf['gopls'].setup({
---   capabilities = capabilities,
---   on_attach = on_attach
--- })
+-- manual setup:
+-- docker pull blinknlights/elastic_ruby_server
+-- docker volume create elastic_ruby_server-0.2.0 (this could be anything but for now match the vscode extension)
+--[[
+  docker run \
+    -d \
+    --rm \
+    --name elastic-ruby-server \
+    --ulimit memlock=-1:-1 \
+    -v elastic_ruby_server-0.2.0:/usr/share/elasticsearch/data \
+    -p 8341:8341 \
+    -e SERVER_PORT=8341 \
+    -e LOG_LEVEL=DEBUG \
+    -e HOST_PROJECT_ROOTS="/Users/kevinhughes/clio" \
+    --mount "type=bind,source=/Users/kevinhughes/clio,target=/projects/clio,readonly" \
+    blinknlights/elastic_ruby_server
+]]
 
--- curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
--- chmod +x ~/.local/bin/rust-analyzer
--- lspconf['rust_analyzer'].setup({
---   capabilities = capabilities,
---   on_attach = on_attach
--- })
+-- define custom language server
+-- local configs = require 'lspconfig/configs'
+-- local util = require 'lspconfig/util'
+--
+-- configs.es_ruby = {
+--   default_config = {
+--     cmd = { 'nc', 'localhost', '8341' },
+--     filetypes = { 'ruby', },
+--     root_dir = util.root_pattern('Gemfile', '.git'),
+--   },
+--   docs = {
+--     description = [[
+--       Language server for Ruby, backed by Elastic search.
+--     ]],
+--     default_config = {
+--       root_dir = [[root_pattern('Gemfile', '.git')]],
+--     },
+--   },
+-- }
 
--- npm install -g pyright
--- lspconf['pyright'].setup({
---   capabilities = capabilities,
---   on_attach = on_attach
--- })
-
--- pip install 'python-lsp-server[all]'
--- lspconf['pylsp'].setup({
---   capabilities = capabilities,
---   on_attach = on_attach
--- })
-
--- npm install -g bash-language-server
--- lspconf['bashls'].setup({
---   capabilities = capabilities,
---   on_attach = on_attach
--- })
-
--- require('config/es_ruby')
 -- lspconf['es_ruby'].setup({
 --   capabilities = capabilities,
 --   on_attach = on_attach
