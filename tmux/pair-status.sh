daa8dc7c3820c1a388d491d38ab6b3ae1f3dac01
Kevin Hughes <kevinhughes27@gmail.com>
2022-05-15 15:26:09 -0400

    rename sep vars

--- tmux/pair-status.sh
+++ tmux/pair-status.sh
@@ -3,11 +3,11 @@
 if pgrep ttyd > /dev/null; then
   color_magenta='#c678dd'
   color_bg='#31353F'
-  right_sep=""
+  rsep=""
 
   pair_style="#[bg=$color_magenta fg=$color_bg bold]"
   pair_style_end="#[bg=$color_bg fg=$color_magenta]"
-  pair_status="$pair_style_end$right_sep$pair_style PAIRING $right_sep$pair_style_end"
+  pair_status="$pair_style_end$rsep$pair_style PAIRING $rsep$pair_style_end"
 
   echo $pair_status
 fi
