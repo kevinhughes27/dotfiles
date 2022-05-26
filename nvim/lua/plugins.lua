5bc2b4976769ce4e9561433838ae9da768b03c08
Kevin Hughes <kevinhughes27@gmail.com>
2022-05-26 12:57:29 -0400

    add nvim lua cmp src

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -190,8 +190,9 @@ return require('packer').startup({function(use)
     requires = {
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
       'saadparwaiz1/cmp_luasnip',
       'onsails/lspkind-nvim',
     },
