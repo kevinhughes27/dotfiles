b2f060630a78cafa3ca6b6896faabb47cbf00146
Kevin Hughes <kevin.hughes@aiven.io>
2022-10-09 12:03:15 -0400

    add smart tab command which moves out of the tree before changing tabs

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,10 +7,23 @@ map('n', '<A-Right>', ':SmartResizeRight 5<CR>')
 map('n', '<A-Up>',    ':SmartResizeUp    5<CR>')
 map('n', '<A-Down>',  ':SmartResizeDown  5<CR>')
 
+-- smart tab next|prev that moved the cursor out of the tree
+-- if necessary. this makes the tabline display a file instead of
+-- NvimTree_ after tabbing away.
+function Smart_tab(tabcmd)
+  local current_buffer = vim.api.nvim_buf_get_name(0)
+
+  if current_buffer:match('NvimTree_%d+') then
+    vim.api.nvim_exec('winc l', true)
+  end
+
+  vim.api.nvim_exec(tabcmd, true)
+end
+
 -- tab for tabs
 -- map('n', '<A-t>', ':tabnew<CR>')
-map('n', '<Tab>', ':tabnext<CR>')
-map('n', '<S-Tab>', ':tabprev<CR>')
+map('n', '<Tab>', function() Smart_tab('tabnext') end)
+map('n', '<S-Tab>', function() Smart_tab('tabprev') end)
 
 -- gimme ctrl s
 map('n', '<C-s>', ':Save<CR>')
