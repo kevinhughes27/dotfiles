7129a05667bf33b39c00b6e816a963ebf2b4efa6
Kevin Hughes <kevin.hughes@aiven.io>
2022-12-25 14:31:53 -0500

    add emoji source

--- nvim/lua/config/nvim-cmp.lua
+++ nvim/lua/config/nvim-cmp.lua
@@ -41,6 +41,7 @@ cmp.setup({
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'emoji' },
     { name = 'path' },
   },
 
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -134,6 +134,7 @@ require('packer').startup({ function(use)
     'hrsh7th/nvim-cmp',
     requires = {
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-emoji',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-cmdline',
       'hrsh7th/cmp-nvim-lsp',
