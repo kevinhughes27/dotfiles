83f179857666cf24e9ff4fa42db62ab9ec34c405
Kevin Hughes <kevinhughes27@gmail.com>
2022-12-27 10:38:28 -0500

    call fzf using vim.fn instead of nvim_exec.
    this fixes the issued with RG by gaining back the ability to use either
    type of quote.
    
    I can also now pass data directly to a `source` so I could clean up
    RecentFiles.
    
    still need to do quickfix and pick a mapping for vsplit that works

--- nvim/lua/config/fzf.lua
+++ nvim/lua/config/fzf.lua
@@ -1,16 +1,18 @@
 local g = vim.g
-local fn = vim.fn
 
+-- actions
 g.fzf_action = {
   ['ctrl-t'] = 'tab split',
   ['ctrl-x'] = 'split',
   ['ctrl-v'] = 'vsplit',
 }
-
 -- need quick fix too
 
+-- preview window
+g.fzf_preview_window = 'right:60%:sharp'
+
 -- layout
-if fn.exists('$TMUX') == 1 then
+if vim.fn.exists('$TMUX') == 1 then
   g.fzf_tmux = 1
   g.fzf_layout = {
     tmux = '-p 90%,90%'
@@ -25,6 +27,7 @@ else
   }
 end
 
+-- toggle tmux layout
 vim.api.nvim_create_user_command("FzfTmuxToggle", function()
   if g.fzf_tmux == 1 then
     g.fzf_tmux = 0
@@ -45,8 +48,7 @@ end, {
   desc = "Toggle Fzf to use a vim window. Needed for pairing to display fzf"
 })
 
-g.fzf_preview_window = 'right:60%:sharp'
-
+-- colors
 g.fzf_colors = {
   fg =      {'fg', 'Normal'},
   bg =      {'bg', 'Normal'},
@@ -59,27 +61,53 @@ g.fzf_colors = {
   spinner = {'fg', 'Label'},
   header =  {'fg', 'Comment'},
 }
-
--- would be nice to define these all the same
 g.fzf_colors['fg+'] = {'fg', 'CursorLine', 'CursorColumn', 'Normal'}
 g.fzf_colors['bg+'] = {'bg', 'CursorLine', 'CursorColumn'}
 g.fzf_colors['hl+'] = {'fg', 'Label'}
 
--- fzf
--- source: can be a vim function or shell escaped string
--- options: shell escaped string
--- https://github.com/junegunn/fzf/blob/master/README-VIM.md
-local function fzf(source, options)
-  local vimscript = string.format([[
-    call fzf#run(fzf#wrap({'source': %s, 'options': '%s'}))
-  ]], source, options)
 
-  vim.api.nvim_exec(vimscript, true)
+-- fzf
+local function fzf(source, options)
+  -- https://github.com/junegunn/fzf/blob/master/README-VIM.md
+  local fzf_run = vim.fn["fzf#run"]
+  local fzf_wrap = vim.fn["fzf#wrap"]
+
+  -- typically fzf_wrap would setup --expects and the sinklist but
+  -- it does so with an anonymous vim function which does not live
+  -- outside of the first vim.fn call meaning it is undefined when
+  -- we call fzf_run. Therefore we have to setup the expected keys
+  -- and sinklist ourselve. fzf_wrap is still useful for the layout
+  -- and colors.
+
+  local keys = {}
+  for k in pairs(g.fzf_action) do keys[#keys + 1] = k end
+  options = options .. " --expect=" .. table.concat(keys, ",")
+
+  local sinklist = function(lines)
+    -- cancelled
+    if next(lines) == nil then return end
+
+    local key = lines[1]
+    local file = lines[2]
+
+    -- action if any
+    local action = g.fzf_action[key]
+    if action then vim.cmd(action) end
+
+    -- open the file
+    vim.cmd("e " .. file)
+  end
+
+  fzf_run(fzf_wrap({
+    source = source,
+    options = options,
+    sinklist = sinklist
+  }))
 end
 
 -- Files
 vim.api.nvim_create_user_command("Files", function()
-  local source = vim.fn.shellescape('') -- FZF_DEFAULT_COMMAND
+  local source = '' -- FZF_DEFAULT_COMMAND
   local options = '--preview "bat --style=numbers --color=always {}"'
   fzf(source, options)
 end, {})
@@ -107,29 +135,28 @@ vim.api.nvim_exec([[
 ]], true)
 
 vim.api.nvim_create_user_command("RecentFiles", function()
-  local source = '_recent_files()'
+  local source = vim.fn["_recent_files"]()
   local options = '--prompt "Recent> " --preview "bat --style=numbers --color=always {}"'
   fzf(source, options)
 end, {})
 
 -- Rg
 vim.api.nvim_create_user_command("Rg", function(args)
-  local source = vim.fn.shellescape('rg --hidden --glob "!.git/*" --column --line-number --color=always --smart-case -- ' .. args.args)
+  local source = 'rg --hidden --glob "!.git/*" --column --line-number --color=always --smart-case -- ' .. args.args
   local options = '--ansi --delimiter : --preview "bat --style=numbers --color=always --highlight-line {2} {1}" --preview-window +{2}-/2'
   fzf(source, options)
 end, { nargs = 1 })
 
 
--- live ripgrep fzf acts as selector only
+-- RG
+-- live ripgrep. fzf acts as selector only
 vim.api.nvim_create_user_command("RG", function(args)
-  -- can't ignore git until I figure out how to call fzf#run without nvim_exec because I need both types of quotes available to me
-  -- --glob "!.git/*"
-  local command_fmt = 'rg --hidden --column --line-number --no-heading --color=always --smart-case -- %s || true'
+  local command_fmt = 'rg --hidden --glob "!.git/*" --column --line-number --no-heading --color=always --smart-case -- %s || true'
   local initial_command = string.format(command_fmt, args.args)
   local reload_command = string.format(command_fmt, '{q}')
 
-  local source = vim.fn.shellescape(initial_command)
-  local options = string.format('--ansi --query "%s" --bind "change:reload:%s"', args.args, reload_command)
+  local source = initial_command
+  local options = string.format("--ansi --query '%s' --bind 'change:reload:%s'", args.args, reload_command)
 
   fzf(source, options)
 end, { nargs = "*" })
