a3b8168a049b391d1f1795422cccecb8c7e64a89
Kevin Hughes <kevin.hughes@aiven.io>
2022-12-28 12:22:05 -0500

    update lua lsp settings

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -18,8 +18,18 @@ lsp_setup.setup({
     sumneko_lua = {
       settings = {
         Lua = {
+          runtime = {
+            version = 'LuaJIT',
+          },
+          diagnostics = {
+            globals = {'vim'},
+          },
           workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
             checkThirdParty = false,
+          },
+          telemetry = {
+            enable = false,
           },
         },
       },
