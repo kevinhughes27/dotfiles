55ba382f9564da6c2d2a2edb9094f21f10de9031
Kevin Hughes <kevinhughes27@gmail.com>
2023-01-17 23:25:40 -0500

    set wezterm padding

--- terminals/wezterm/wezterm.lua
+++ terminals/wezterm/wezterm.lua
@@ -2,17 +2,27 @@ local wezterm = require "wezterm"
 
 return {
   color_scheme = "OneDark (base16)",
+
   font = wezterm.font_with_fallback {
     "Hack Nerd Font",
     "Noto Color Emoji",
   },
   font_size = 11,
+
   enable_tab_bar = false,
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 2,
+    bottom = 0,
+  },
   window_background_opacity = 1.0,
+
   keys = {
     { key = 'v', mods = 'CTRL', action = wezterm.action.Paste },
     { key = 'F11', action = wezterm.action.ToggleFullScreen },
   },
+
   mouse_bindings = {
     -- Change the default click behavior so that it populates
     -- the Clipboard rather the PrimarySelection.
@@ -22,5 +32,6 @@ return {
       action = wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor 'Clipboard',
     },
   },
+
   audible_bell = "Disabled",
 }
