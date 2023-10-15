f13830773ce9ab30bb566e1bea1128453523ddd7
Kevin Hughes <kevinhughes27@gmail.com>
2023-10-15 13:54:08 -0400

    update tmux osc52 config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -181,7 +181,7 @@ return {
   {
     'ojroques/nvim-osc52',
     config = function()
-      require('osc52').setup({ silent = true })
+      require('osc52').setup({ silent = true, tmux_passthrough = true })
     end
   },
 
