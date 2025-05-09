bc126b5dacdca20b07644e90917817cade388e6c
Kevin Hughes <kevin.hughes@aiven.io>
2025-05-09 11:41:46 -0400

    update plugins. markdown and mason 2.0

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,70 +1,62 @@
--- references:
--- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/lsp.md#you-might-not-need-lsp-zero
--- https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua#L405
---
--- pylsp requires `python3-venv` package and fails with an unclear message without it
---
 return {
   {
     'neovim/nvim-lspconfig',
     dependencies = {
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
-      'saghen/blink.cmp',
     },
     config = function()
-      require('mason').setup({
-        PATH = 'append',
-      })
-
-      local servers = {
-        gopls = {},
-        pylsp = {},
-        ruff = {},
-        ts_ls = {},
-        lua_ls = {
+      vim.lsp.config('lua_ls', {
+        settings = {
           Lua = {
-            workspace = { checkThirdParty = false },
-            telemetry = { enable = false },
+            runtime = {
+              version = 'LuaJIT',
+            },
+            diagnostics = {
+              globals = {
+                'vim',
+                'require',
+              },
+            },
           },
         },
-      }
+      })
 
-      local on_attach = function(client, bufnr)
-        if client.name == 'ruff' then
-          -- disable hover in favor of pylsp
-          client.server_capabilities.hoverProvider = false
-        end
+      -- pylsp requires `python3-venv` package and fails with an unclear message without it
+      vim.lsp.config('pylsp', {
+        cmd = { 'pylsp' },
+        root_markers = { 'pyproject.toml', 'requirements.txt', '.git' },
+        filetypes = { 'python' },
+      })
 
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-        vim.keymap.set('n', 'ca', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-        vim.keymap.set('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-        vim.keymap.set('n', '<RightMouse>', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+      vim.lsp.config('ruff', {
+        cmd = { 'ruff', 'server' },
+        root_markers = { 'pyproject.toml', 'ruff.toml', '.ruff.toml', '.git' },
+        filetypes = { 'python' },
+      })
 
-        vim.diagnostic.config({virtual_text = true})
-      end
+      vim.lsp.config('gopls', {
+        cmd = { 'gopls' },
+        root_markers = { 'go.mod', '.git' },
+        filetypes = { 'go' },
+      })
 
-      local lspconfig = require('lspconfig')
-      local mason_lspconfig = require('mason-lspconfig')
+      -- on attach
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(ev)
+          local opts = { noremap = true, silent = true, buffer = ev.buf }
 
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
+          vim.keymap.set('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+          vim.keymap.set('n', 'ca', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+          vim.keymap.set('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+          vim.keymap.set('n', '<RightMouse>', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
 
-      mason_lspconfig.setup {
-        ensure_installed = vim.tbl_keys(servers),
-      }
-
-      mason_lspconfig.setup_handlers {
-        function(server_name)
-          lspconfig[server_name].setup {
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = servers[server_name],
-            filetypes = (servers[server_name] or {}).filetypes,
-          }
+          vim.diagnostic.config({virtual_text = true})
         end,
-      }
+      })
+
+      require("mason").setup()
+      require("mason-lspconfig").setup()
     end,
   },
 }
