cded60c388c16ef978cc6052a0ef407f8b3b0b5b
Kevin Hughes <kevinhughes27@gmail.com>
2025-06-09 09:58:40 -0400

    setup vim-test for vitest

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -7,6 +7,7 @@ return {
     init = function()
       vim.g['test#strategy'] = 'vimux'       -- make test commands execute using vimux
       vim.g['test#python#runner'] = 'pytest' -- have to configure which python runner to use https://github.com/vim-test/vim-test#python
+      vim.g['test#javascript#runner'] = 'vitest'
       vim.g['VimuxUseNearest'] = 1
       vim.g['VimuxHeight'] = '25'
     end,
