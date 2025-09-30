7227be3b0858ecbbaecb6f0d6386259d87c9ba58
Kevin Hughes <kevin.hughes@aiven.io>
2025-09-30 11:13:27 -0400

    try to fix revive linting. seems it is fixed without these changes now...?

--- nvim/lua/plugins/golang.lua
+++ nvim/lua/plugins/golang.lua
@@ -3,7 +3,10 @@ return {
     'crispgm/nvim-go',
     config = function()
       require('go').setup({
-        lint_prompt_style = 'vt'
+        lint_prompt_style = 'vt',
+        -- linter_flags = {
+        --   revive = {'-config', '~/revive.toml'}
+        -- },
       })
     end
   },
